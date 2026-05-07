4ff83afa7b393fe8070133d112166f710e1e41e6
Neoscripter <sange0337@gmail.com>
2026-05-07 17:15:41 +0800

    Style:add fade in animation on stage card text

--- client/src/pages/about/partials/StageItem.tsx
+++ client/src/pages/about/partials/StageItem.tsx
@@ -36,8 +36,8 @@ const StageItem: FC<{
             >
                 <stage.icon class="size-8" />
             </div>
-            <h3 class="mb-3 text-xl font-bold xl:mb-4 xl:text-[1.325rem]">{`${order}. ${stage.title[lang]}`}</h3>
-            <p class="text-sm sm:text-base xl:text-lg">
+            <h3 key={`${lang}-heading`} class="motion-safe:animate-fade-in mb-3 text-xl font-bold xl:mb-4 xl:text-[1.325rem]">{`${order}. ${stage.title[lang]}`}</h3>
+            <p key={`${lang}-content`} class="motion-safe:animate-fade-in text-sm sm:text-base xl:text-lg">
                 {stage.description[lang]}
             </p>
         </li>
