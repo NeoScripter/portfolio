11c711fb6ca0c7c3eaac4aece02db14c2c9ccd55
Neoscripter <sange0337@gmail.com>
2026-05-09 12:23:44 +0800

    Style:write featured quote

--- client/src/pages/about/partials/FeaturedQuote.tsx
+++ client/src/pages/about/partials/FeaturedQuote.tsx
@@ -2,6 +2,7 @@ import Quote from '@/assets/svgs/Quote';
 import AppSection from '@/layouts/SectionLayout';
 import { cn } from '@/lib/helpers/utils';
 import type { NodeProps } from '@/lib/types/shared';
+import { locale } from '@/signals/locale';
 import type { FC } from 'preact/compat';
 
 const FeaturedQuote: FC<{ className?: string }> = ({ className }) => {
@@ -13,11 +14,13 @@ const FeaturedQuote: FC<{ className?: string }> = ({ className }) => {
             )}
         >
             <blockquote class="relative isolate m-auto mx-auto max-w-9/10 sm:max-w-147 lg:max-w-208 2xl:max-w-307">
-                <p class="text-center text-balance sm:text-xl xl:text-3xl 2xl:text-4xl">
-                    Я специализируюсь на создании качественных и надежных
-                    сайтов. За годы работы я помог многим клиентам реализовать
-                    их проекты — от простых портфолио до сложных
-                    интернет-магазинов.
+                <p
+                    key={`${locale.value}-quote`}
+                    class="motion-safe:animate-fade-in mx-auto 2xl:max-w-9/10 text-center text-balance sm:text-xl xl:text-3xl 2xl:text-4xl"
+                >
+                    {locale.value === 'en'
+                        ? 'Nothing worthwhile comes easily. Half effort does not produce half results. It produces no results. Continuous and hard work is the only way to accomplish results that last.'
+                        : 'Любая вещь, имеющая ценность, требует приложенных усилий. Половина приложенных усилий не означает получение половины результата. Она означает отсутствие результата. Единственный способ получения качественного результата - это кропотливая и упорная работа.'}
                 </p>
 
                 <QuoteGlyph className="-right-10 -bottom-20 xl:-right-20 2xl:-right-5" />
