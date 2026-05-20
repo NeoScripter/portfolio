9307b1cd1674b5a746cd1285b60621986d7e8fa8
Neoscripter <sange0337@gmail.com>
2026-05-20 23:26:10 +0800

    Style:increase the margin bottom in the video heading

--- client/src/pages/home/partials/Videos.tsx
+++ client/src/pages/home/partials/Videos.tsx
@@ -72,7 +72,7 @@ const Videos = () => {
             >
                 <SecondaryHeading
                     key={`${locale.value}-heading`}
-                    className="motion-safe:animate-fade-in text-balance"
+                    className="motion-safe:animate-fade-in mb-20 sm:mb-20 xl:mb-20"
                 >
                     {locale.value === 'en'
                         ? 'Live coding videos'
